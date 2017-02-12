5c18d33a235f95927c2d5354261b2ccc2cd854bc
ikorgik <ikorgik@gmail.com>
2017-02-12 21:21:03 +0300

    #2849175 Added default layout.

--- src/Plugin/views/exposed_form/VeflBasic.php
+++ src/Plugin/views/exposed_form/VeflBasic.php
@@ -3,6 +3,7 @@
 namespace Drupal\vefl\Plugin\views\exposed_form;
 
 use Drupal\views\Plugin\views\exposed_form\Basic;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Exposed form plugin that provides a basic exposed form with layout.
@@ -16,5 +17,60 @@ use Drupal\views\Plugin\views\exposed_form\Basic;
  * )
  */
 class VeflBasic extends Basic {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['layout'] = array(
+      'contains' => array(
+        'layout_id' => array('default' => 'vefl_onecol'),
+        'regions' => array('default' => array()),
+        'widget_region' => array('default' => array()),
+      ),
+    );
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    $options = vefl_get_layouts();
+    $layout_id = $this->options['layout']['layout_id'];
+
+    // Outputs layout selectbox.
+    $form['layout'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Layout settings'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+    );
+    $form['layout']['layout_id'] = array(
+      '#prefix' => '<div class="container-inline">',
+      '#type' => 'select',
+      '#options' => $options,
+      '#title' => t('Layout'),
+      '#default_value' => $layout_id,
+    );
+    $form['layout']['change'] = array(
+      '#type' => 'submit',
+      '#value' => t('Change'),
+      '#submit' => array('_vefl_change_layout_button'),
+      '#suffix' => '</div>',
+    );
+
+    // Go through each filter and add BEF options.
+    foreach ($this->view->display_handler->getHandlers('filter') as $label => $filter) {
+      if (!$filter->options['exposed']) {
+        continue;
+      }
+
+
+    }
+  }
 
 }
